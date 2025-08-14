b8eda4b787aa7b73f709890a2271ce81d744a422
youssef <youssefmina2006@gmail.com>
2025-08-15 00:50:03 +0300

    home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import{NaviBarHome} from './navi-bar-home/navi-bar-home';
+import{MainHome} from './main-home/main-home';
+import{FooterHome} from './footer-home/footer-home';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NaviBarHome, MainHome,FooterHome],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/navi-bar-home/navi-bar-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navi-bar-home',
+  imports: [],
+  templateUrl: './navi-bar-home.html',
+  styleUrl: './navi-bar-home.css'
+})
+export class NaviBarHome {
+
+}
